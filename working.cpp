a3435716a6f2dd780068cd3eb320f17a197b2a83
muraliraghavendra240 <94122894+muraliraghavendra240@users.noreply.github.com>
2023-02-18 17:01:49 +0530

    Add files via upload

---
+++ working.cpp
@@ -0,0 +1,10 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkin\n";
+  cout << "using jenkins pipeline\n";
+  return 0;
+}
